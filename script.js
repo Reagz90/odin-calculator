5e8f76e95fcf61037bcafee79dad9d445cf99825
Reagz90 <reagan.hammond90@gmail.com>
2023-04-10 16:21:43 +1000

    added operate function step 3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+//variables for user input, 1st number, 2nd number & operator
+let firstNumber; 
+let secondNumber; 
+let operator; 
 
 //Calculator functions for basic math operators
 function add(num1, num2) {
@@ -15,4 +18,20 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2; 
+}
+
+/*operate function, switch statement calls appropriate math function
+depending on operator passed in*/
+
+function operate(operator, num1, num2) { 
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2); 
+        case '/':
+            return divide(num1, num2); 
+    }
 }
